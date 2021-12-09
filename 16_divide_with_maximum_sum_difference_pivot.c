74cf1c1defdba20ae95eebfa2c0be038de040e4d
chenbohuo <chenboshuo.hi@163.com>
2021-12-09 17:40:33 +0800

    :rocket: 改良的快速排序

--- 16_divide_with_maximum_sum_difference_pivot.c
+++ 16_divide_with_maximum_sum_difference_pivot.c
@@ -9,7 +9,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int comp(const void *a, const void *b) { return *(int *)a - *(int *)b; }
+void exchange(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+/**
+ * 使数组nums[left..right]有序，
+ * @param  nums  数组
+ * @param  left  左边的下标
+ * @param  right 右边界下标
+ * @return       枢纽的位置
+ */
+int parition(int *nums, int left, int right) {
+  int right_value = nums[right];
+  int less = left - 1;
+  for (int i = left; i < right; ++i) {
+    if (nums[i] <= right_value) {
+      ++less;
+      exchange(&nums[less], &nums[i]);
+    }
+  }
+  exchange(&nums[less], &nums[right]);
+  return less + 1;
+}
+
+/**
+ * 部分快速排序,只对当前元素大的部分做快速排序
+ * @param  nums   数组
+ * @param  midd   数组中间的位置
+ * @param  left   最左边的下标
+ * @param  right  最右边的下标
+ */
+void partly_quick_sort(int *nums, int mid, int left, int right) {
+  int pivot = parition(nums, left, right);
+  if (pivot > mid) {
+    partly_quick_sort(nums, mid, left, pivot - 1);
+  } else if (pivot < mid) {
+    partly_quick_sort(nums, mid, pivot + 1, right);
+  }
+}
 
 /**
  * 返回划分后S2开始的位置
@@ -18,8 +58,9 @@ int comp(const void *a, const void *b) { return *(int *)a - *(int *)b; }
  * @return        划分之后后半部分起点位置
  */
 int divide_with_maximum_sum_difference(int *nums, int length) {
-  qsort(nums, sizeof(int), length, comp);
-  return length / 2;
+  int mid = length / 2;
+  partly_quick_sort(nums, mid, 0, length - 1);
+  return mid;
 }
 
 int main() {
@@ -41,5 +82,15 @@ int main() {
    * input: [1,2,3,4,5,6]
    * expect:3
    * output:3
+   */
+
+  int test3[5] = {0,1,4,3,2};
+  printf("input:[0,1,4,3,2]\n");
+  printf("expect:2\n");
+  printf("output:%d\n", divide_with_maximum_sum_difference(test3, 5));
+  /**
+   * input:[0,1,4,3,2]
+   * expect:2
+   * output:2
    */
 }
